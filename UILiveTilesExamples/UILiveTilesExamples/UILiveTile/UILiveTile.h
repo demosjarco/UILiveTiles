45de8d2e082cf3c3549fe66b30e5a505bc548f57
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-04-15 00:00:23 -0700

    Initial Build

--- UILiveTilesExamples/UILiveTilesExamples/UILiveTile/UILiveTile.h
+++ UILiveTilesExamples/UILiveTilesExamples/UILiveTile/UILiveTile.h
@@ -1,9 +1,9 @@
 //
 //  UILiveTile.h
-//  
+//  UILiveTilesExamples
 //
 //  Created by Victor Ilisei on 4/14/14.
-//
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
