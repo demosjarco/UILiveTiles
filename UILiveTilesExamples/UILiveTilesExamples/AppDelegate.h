eb1d411c4ec7157d1755c14342d0c87057a1c1af
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-04-14 23:53:30 -0700

    Initial Build

---
+++ UILiveTilesExamples/UILiveTilesExamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UILiveTilesExamples
+//
+//  Created by Victor Ilisei on 4/14/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UILiveTilesExamples/UILiveTilesExamples/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  UILiveTilesExamples
+//
+//  Created by Victor Ilisei on 4/14/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
